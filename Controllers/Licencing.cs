29f37f781b66098d811d331f626b8e6c00766384
Mike Sheen <mike@sheen.id.au>
2025-08-22 09:01:24 +0800

    Remove rubble

--- Controllers/Licencing.cs
+++ Controllers/Licencing.cs
@@ -14,28 +14,10 @@ namespace JiwaCustomerPortal.Controllers
         [HttpGet]
         public async Task<IActionResult> Getdownloadlicences(string CompanyName, string Version)
         {
-            //var data = new { Name = "Blazor", Framework = "ASP.NET Core", Version = "9.0" };
-            //return Ok(data); // Returns JSON
-
             LicenceRenewGETRequest licenceRenewGETRequest = new LicenceRenewGETRequest() { CompanyName = CompanyName, Version = Version };
             List<JiwaFinancials.Jiwa.JiwaServiceModel.Licencing.Licence> licences = await JiwaAPI.GetAsync(licenceRenewGETRequest, jiwaAPIKey: Config.JiwaAPIKey);
 
-            return Ok(licences);
-            //try
-            //{
-            //    salesOrder = await JiwaAPI.GetAsync(salesOrderGETRequest, WebPortalUserSessionStateContainer.WebPortalUserSession.Id, null);
-            //    SelectedHistory = salesOrder.Histories[0];
-
-            //    if (QueryHelpers.ParseQuery(NavigationManager.ToAbsoluteUri(NavigationManager.Uri).Query).TryGetValue("SnapshotNo", out var snapshotNoParam))
-            //    {
-            //        short snapno = short.Parse(snapshotNoParam);
-
-            //        if (snapno <= salesOrder.Histories.Count)
-            //        {
-            //            SelectedHistory = salesOrder.Histories[snapno - 1];
-            //        }
-            //    }
-            //}
+            return Ok(licences);            
         }
     }
 }
